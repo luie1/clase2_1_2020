459d6c63f1efa1c46c7e9e85b682798a27645077
luis alfonso <lruiseacm@gmail.com>
2020-07-30 14:56:41 -0400

    segundo commit

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb+srv://luis:luis123@cluster0-7er3y.mongodb.net/test').then(()=>{
+mongoose.connect('mongodb+srv://luis:appsis719@cluster0-7er3y.mongodb.net/test').then(()=>{
   console.log('succes db');
 }).catch((err)=>{
   console.log(err);
